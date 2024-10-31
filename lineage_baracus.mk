14a6ce1ac1171d53df0ee6005bfedd1f9d5071eb
Aaron Kling <webgeek1234@gmail.com>
2024-10-31 12:56:46 -0500

    Initial commit
    This is a shim repo to android_device_nvidia_foster, where baracus
    is supported as part of the unified build. This separate build is
    needed due to a u-boot chainload being required for a larger kernel
    image.

---
+++ lineage_baracus.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+
+# Inherit device configuration for baracusshield.
+include device/nvidia/foster/lineage.mk
+$(call inherit-product, device/google/baracusshield/full_baracusshield.mk)
+
+PRODUCT_NAME := lineage_baracusshield
+PRODUCT_DEVICE := baracusshield
